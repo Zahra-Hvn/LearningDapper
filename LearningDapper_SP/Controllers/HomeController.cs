53ff91bb3982d2790d9171fa25af0b5bf85bde49
z.havadanloo <z.havadanloo@DESKTOP-TIFGCRT>
2018-12-06 10:32:37 +0330

    edit

--- LearningDapper_SP/Controllers/HomeController.cs
+++ LearningDapper_SP/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LearningDapper_SP.Models;
+using LearningDapper_SP.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,48 @@ namespace LearningDapper_SP.Controllers
 {
     public class HomeController : Controller
     {
+        UserRepo userRepo;
+        public HomeController()
+        {
+            userRepo = new UserRepo();
+        }
         // GET: Home
         public ActionResult Index()
         {
+            return View(userRepo.GetUsers());
+        }
+        public ActionResult Edit(int id)
+        {
+            var model = userRepo.GetUserById(id);
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Edit(User user)
+        {
+            if (userRepo.UpdateUser(user))
+            {
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+        public ActionResult Delete(int id)
+        {
+            userRepo.DeleteUser(id);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Insert()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Insert(User user)
+        {
+            if (userRepo.InsertUser(user))
+            {
+                return RedirectToAction("Index");
+            }
+            return View(user);
+
         }
     }
 }
--- LearningDapper_SP/Repository/UserRepo.cs
+++ LearningDapper_SP/Repository/UserRepo.cs
@@ -6,7 +6,8 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using Dapper;
-
+using System.Data;
+using static Dapper.SqlMapper;
 
 namespace LearningDapper_SP.Repository
 {
@@ -14,14 +15,69 @@ namespace LearningDapper_SP.Repository
     {
         private string _ConnectionString = ConfigurationManager.ConnectionStrings["ConString"].ConnectionString;
 
-        public User GetUsers()
+        public List<User> GetUsers()
         {
-            using (var con=new SqlConnection(_ConnectionString))
+            using (var con = new SqlConnection(_ConnectionString))
             {
-                //con.Query<>
+                return con.Query<User>("GetUsers", CommandType.StoredProcedure).ToList();
             }
-           
         }
-
+        public User GetUserById(int id)
+        {
+            using (var con = new SqlConnection(_ConnectionString))
+            {
+                //IDynamicParameters 
+                return con.Query<User>("GetUserById", new { id }, commandType: CommandType.StoredProcedure).FirstOrDefault();
+            }
+        }
+        public bool UpdateUser(User user)
+        {
+            using (var con = new SqlConnection(_ConnectionString))
+            {
+                try
+                {
+                    con.Execute("UpdateUser", user, commandType: CommandType.StoredProcedure);
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+        }
+        public bool DeleteUser(int id)
+        {
+            using (var con = new SqlConnection(_ConnectionString))
+            {
+                try
+                {
+                    con.Execute("DeleteUser", new { id }, commandType: CommandType.StoredProcedure);
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+        }
+        public bool InsertUser(User user)
+        {
+            using (var con = new SqlConnection(_ConnectionString))
+            {
+                try
+                {
+                    DynamicParameters param = new DynamicParameters();
+                    param.Add("@Name",user.Name);
+                    param.Add("@Family",user.Family);
+                    param.Add("@Mail",user.Mail);
+                    con.Execute("AddUser", param, commandType: CommandType.StoredProcedure);
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+        }
     }
 }
